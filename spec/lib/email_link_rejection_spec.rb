e6dfc2e9582e7efc82a9f7d7d656052bb1875b8d
Bryce McDonnell <bryce@bridgetownint.com>
2016-07-01 13:48:50 -0400

    WIP - moving link rejection to its own class

---
+++ spec/lib/email_link_rejection_spec.rb
@@ -0,0 +1,75 @@
+require 'spec_helper'
+
+RSpec.describe EmailLinkRejection do
+  let(:el) {
+    el = EmailLink.new
+    el.id = 'abc'
+    el.title = 'hello world'
+    el
+  }
+
+  let(:elr) {
+    EmailLinkRejection.new(el)
+  }
+
+  describe 'init' do
+    it 'takes an EmailLink object to initialize' do
+      expect {
+        EmailLinkRejection.new
+      }.to raise_error(ArgumentError)
+      expect {
+        EmailLinkRejection.new([])
+      }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe 'rejectable?' do
+    it 'returns true if the link should not be rejected' do
+      expect_any_instance_of(EmailLinkRejection).to receive(:find_duplicated_links).
+        twice.and_return(false)
+
+      expect(elr.rejectable?).to be_falsey
+      expect(elr.reject_reasons).to be_empty
+    end
+
+    it 'rejects if number of words in title is below a threshold' do
+      expect_any_instance_of(EmailLinkRejection).to receive(:find_duplicated_links).
+        twice.and_return(false)
+
+      el.cnt_title_words = (EmailLinkRejection::REJECT_WORD_COUNT_THRESHOLD - 1)
+      elr = EmailLinkRejection.new(el)
+
+      expect(elr.rejectable?).to be_truthy
+      expect(elr.reject_reasons).to eql([:title_threshold])
+    end
+
+    it 'rejects if link title contains some magic reject words' do
+      expect_any_instance_of(EmailLinkRejection).to receive(:find_duplicated_links).
+        twice.and_return(false)
+
+      el.title = 'unsubscribe from this email'
+      elr = EmailLinkRejection.new(el)
+      expect(elr.rejectable?).to be_truthy
+      expect(elr.reject_reasons).to eql([:phrase])
+    end
+
+    it 'rejects if link title has been considered before' do
+      expect_any_instance_of(EmailLinkRejection).to receive(:find_duplicated_links).
+        and_return(true)
+
+      expect(elr.rejectable?).to be_truthy
+      expect(elr.reject_reasons).to eql([:duplicate_link])
+    end
+
+    it 'rejects if link title contains some magic reject words' do
+      expect_any_instance_of(EmailLinkRejection).to receive(:find_duplicated_links).
+        and_return(true)
+      el.title = 'unsubscribe'
+      el.cnt_title_words = (EmailLinkRejection::REJECT_WORD_COUNT_THRESHOLD - 1)
+      elr = EmailLinkRejection.new(el)
+
+      expect(elr.rejectable?).to be_truthy
+      expect(elr.reject_reasons).to eql([:title_threshold, :phrase, :duplicate_link])
+    end
+  end
+end
