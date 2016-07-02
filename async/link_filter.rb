23158a8174eb2e64e7a2c3f34ffda4db6921737f
Bryce McDonnell <bryce@bridgetownint.com>
2016-07-02 12:15:16 -0400

    BUGFIX - handle 404 in LinkFilter
    Was filling up log files with worthless information. Changed to single
    log line so we search for how often this error happens, but otherwise
    async task can halt retries

--- async/link_filter.rb
+++ async/link_filter.rb
@@ -2,16 +2,17 @@ class LinkFilter
   include Sidekiq::Worker
 
   def perform(email_link_id)
-    el = EmailLink.find(email_link_id)
+    begin
+      el = EmailLink.find(email_link_id)
+    rescue Elasticsearch::Transport::Transport::Errors::NotFound => e
+      puts "[ERROR] LinkFilter can not find #{email_link_id}"
+      return false
+    end
+
     el.cnt_title_words = word_count(el.title)
     result = el.update
     el.check_and_reject
 
-    # FIXME / TODO:
-    # reenable this infrastructure
-    #if autoreject?(el)
-      #EmailLink.reject_from_reading_list(email_link_id)
-    #end
     result
   end
 
@@ -23,12 +24,5 @@ class LinkFilter
     rescue # if it doesn't respond to #split, I don't want to know it
       0
     end
-  end
-
-  def autoreject?(el)
-    resp = HTTParty.post( "#{ENV['PIO_FILTER_SERVER_URL']}/queries.json",
-                 body: {text: el.title}.to_json,
-                 headers: { 'Content-Type' => 'application/json' })
-    resp["category"] == "reject"
   end
 end
